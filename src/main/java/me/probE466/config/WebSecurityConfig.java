49123d0b230532886e767e1cf39be5da93be6d83
probE466 <lars.grahmann@yahoo.de>
2016-10-12 18:15:33 +0200

    added mvc config

--- src/main/java/me/probE466/config/WebSecurityConfig.java
+++ src/main/java/me/probE466/config/WebSecurityConfig.java
@@ -3,24 +3,26 @@ package me.probE466.config;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 
 //@EnableWebSecurity
 //@Configuration
 public class WebSecurityConfig  {
 
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-////        auth
-////                .inMemoryAuthentication()
-////                .withUser("user").password("password").roles("ADMIN");
-//    }
-//
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().ignoringAntMatchers("/post");
-//        http.authorizeRequests()
-//                .antMatchers("/admin/**")
-//                .authenticated()
-//                .antMatchers("/**").permitAll().and().httpBasic();
-//    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user").password("password").roles("ADMIN");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/post");
+        http.authorizeRequests()
+                .antMatchers("/admin/**")
+                .authenticated()
+                .antMatchers("/**").permitAll().and().httpBasic();
+    }
 }
