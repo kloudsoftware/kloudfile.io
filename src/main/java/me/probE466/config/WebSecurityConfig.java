6d50a3b97e6843e0f44f6b727b0fac42233520b1
probE466 <lars.grahmann@yahoo.de>
2016-10-12 18:28:40 +0200

    added config

--- src/main/java/me/probE466/config/WebSecurityConfig.java
+++ src/main/java/me/probE466/config/WebSecurityConfig.java
@@ -6,11 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig  {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -19,6 +20,7 @@ public class WebSecurityConfig  {
 //                .withUser("user").password("password").roles("ADMIN");
     }
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/post");
         http.authorizeRequests()
